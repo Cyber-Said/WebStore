b710e9372c33710084236fd02c91232c81931080
WebStore Dev <dev@webstore.com>
2026-02-27 10:24:50 +0600

    feat(product): add Product viewset and routes

--- config/api_urls.py
+++ config/api_urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from product.views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='products')
 
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
