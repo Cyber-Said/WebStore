31844afae0f37ab9df3eb1b574754b92392b9078
WebStore Dev <dev@webstore.com>
2026-02-27 10:27:24 +0600

    feat(product): restrict write actions to admin

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
 
-# Create your views here.
+from .models import Product
+from .permissions import IsAdminOrReadOnly
+from .serializers import ProductSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().order_by("-id")
+    queryset = Product.objects.all().order_by('-id')
     serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
