974b745098bde3e3912f4d396da4298d6b5baf37
WebStore Dev <dev@webstore.com>
2026-03-03 22:05:04 +0600

    chore(cart): add REDIS_URL and redis client helper

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -133,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
